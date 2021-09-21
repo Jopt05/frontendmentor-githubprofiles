aacd2a88b1ab07c73138d7b2f89ba9044a50d5bf
Jesus <jopt98@gmail.com>
2021-09-20 20:01:49 -0500

    Added animation when loading

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { appContext } from '../HighOrderComponents/Context'
 import InformationBiography from './InformationBiography'
 import InformationExtra from './InformationExtra'
 import InformationHeader from './InformationHeader'
 import InformationStats from './InformationStats'
 
 const Information = () => {
+
+  const { Info } = useContext(appContext);
+
+  useEffect(() => {
+    document.querySelector(".Information").classList.add("goIn");
+
+    setTimeout(() => {
+      document.querySelector(".Information").classList.remove("goIn");
+    }, 900);
+  },[ Info ])
+
     return (
         <div className="Information">
           <InformationHeader />
